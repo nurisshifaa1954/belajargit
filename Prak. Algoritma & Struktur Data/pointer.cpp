fa8b649f8f077d622d5b15396ad3377d48866d21
nurisshifa <124240186@student.upnyk.ac.id>
2025-02-15 13:56:36 +0700

    menambahkan file dokumen

---
+++ Prak. Algoritma & Struktur Data/pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// 87, 56, 93, 70, 100
+int array[5] = {87, 56, 93, 70, 100}
+
+int
+main()
+{
+    int array[5] = {87, 56, 93, 70, 100}
+
+    // cetak array
+    for (int i = 0; i < 5; i++)
+    {
+    }
+
+    int max = *array,
+        min = *array;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *(array + i) << endl:
+    }
+}
